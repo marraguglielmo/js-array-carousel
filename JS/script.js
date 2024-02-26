3ffdff24c14ed93d7a98b996a48e3f8965f9b0b8
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-26 15:51:05 +0100

    [add] punti readme

--- JS/script.js
+++ JS/script.js
@@ -10,14 +10,19 @@ const images = [
     'img/04.webp',
     'img/05.webp',
 ]
-console.log(images);
 
 //2.
-
 for(let i = 0; i < images.length; i++){
 
-    imgWrapper.innerHTML += `<img class="d-none" src="${images[i]}">`
+    imgWrapper.innerHTML += `<img class="img d-none" src="${images[i]}">`
     
 }
 
+// 3.  prendo tutti gli elementi con la classe img
+const imgCollection = document.getElementsByClassName("img");
 
+// rimuovo la classe d-none alla prima immagine
+imgCollection[0].classList.remove("d-none");
+
+// 4. inizializzo il counter
+const counter = 0;
